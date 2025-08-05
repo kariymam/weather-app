4669b2986542b13e7f6d052b853a408db1504e2e
kariymam <59487728+kariymam@users.noreply.github.com>
2025-08-05 17:50:43 -0400

    Remove unused types

--- types.ts
+++ types.ts
@@ -6,14 +6,6 @@ export interface Geolocation {
 	};
 }
 
-export type WeatherApiResponse = {
-	location: Geolocation;
-	current: any;
-	periodsByHour: any[];
-	periodsByDay: any[];
-	alerts: any[];
-};
-
 export interface GeolocationNavigatorError {
 	name: string;
 	code: number;
@@ -56,55 +48,30 @@ export type MapboxResponse = {
 	attribution: 'NOTICE: © 2025 Mapbox and its suppliers. All rights reserved. Use of this data is subject to the Mapbox Terms of Service (https://www.mapbox.com/about/maps/). This response and the information it contains may not be retained.';
 };
 
-export interface WeatherGovGeoJson {
-  '@context'?: [
-    string,
-    {
-      '@version': string
-      wx: string;
-      '@vocab': string;
-    }
-  ];
-  type?: 'FeatureCollection';
-  geometry?: { type: 'Polygon', coordinates: [number, number] },
-  properties?: {
-        units: 'us',
-        forecastGenerator: 'BaselineForecastGenerator',
-        generatedAt: string,
-        updateTime: string,
-        validTimes: string,
-        elevation: object[],
-        periods: WeatherGovPeriods[]
-      },
-  features?: [
-    {
-      id: string,
-      type: 'Feature',
-      geometry: null,
-      properties?: WeatherGovAlert;
-    }],
-  title?: string;
-  updated?: string;
-}
+export type WeatherApiResponse = {
+	coordinates: string;
+	current: openmeteo['current'];
+	periods: openmeteo['periods'];
+	alerts: [] | WeatherGovAlert[];
+};
 
-export type WeatherGovPeriods = {
-	number: number;
-	name: string;
-	startTime: string;
-	endTime: string;
-	isDaytime: boolean;
-	temperature: number;
-	temperatureUnit: string;
-	temperatureTrend?: string;
-	probabilityOfPrecipitation: {
-		unitCode: string;
-		value: number;
+export type openmeteo = {
+	current: {
+		time: Date;
+		temperature2m: number;
+		precipitation: number;
+		isDay: number;
+		apparentTemperature: number;
 	};
-	windSpeed: string;
-	windDirection: string;
-	icon: string;
-	shortForecast: string;
-	detailedForecast: string;
+	periods: {
+		time: Date;
+		temperature2m: number;
+		apparentTemperature: number;
+		precipitationProbability: number;
+		showers: number;
+		rain: number;
+		snowfall: number;
+	}[];
 };
 
 export interface WeatherGovAlert {
@@ -159,48 +126,3 @@ export interface WeatherGovAlert {
     NationalWeatherService: string[];
   };
 }
-
-export interface OpenmeteoResponse {
-	latitude: number;
-	longitude: number;
-	timezone: string;
-	current: {
-		time: string;
-		temperature2m: number;
-		rain: number;
-		apparentTemperature?: number;
-		precipitation?: number | null | undefined | Float32Array<ArrayBufferLike>;
-	};
-	periods: OpenmeteoResponsePeriod[];
-};
-
-export type WeathergovResponse = {
-		forecast: WeatherGovProperties;
-		forecastHourly: WeatherGovProperties;
-		alerts: [] | WeatherGovAlert[];
-	};
-
-export type OpenmeteoResponsePeriod = {
-	time: string;
-	temperature2m: number;
-	apparentTemperature?: number;
-	precipitationProbability?: number;
-  showers: number;
-  rain: number;
-  snowfall: number;
-};
-
-export type WeatherGovProperties = {
-	units: string;
-	forecastGenerator: 'BaselineForecastGenerator' | 'HourlyForecastGenerator';
-	generatedAt: string;
-	updateTime: string;
-	validTimes: string;
-	elevation: object[];
-	periods: WeatherGovPeriods[];
-};
-
-export type WeatherSources = {
-	openmeteo: OpenmeteoResponse;
-	weathergov: WeathergovResponse;
-};
