8787833443e9435e6e1ad7dac3d36aea4fb827c0
kariymam <59487728+kariymam@users.noreply.github.com>
2025-10-21 22:35:24 -0400

    spacing

--- components/ui/header.vue
+++ components/ui/header.vue
@@ -1,23 +1,23 @@
 <template>
   <header id="main-header">
-				<AppNavbar>
-					<template #home>
-						Weather App
-					</template>
-					<template #location-settings>
-						<location-settings-dialog
-							:location="UserLocation"
-							:locations="getLocationHistory"
-							:use-search-a-p-i="getMapboxSearchResponse"
-						/>
-					</template>
-					<template #weather>
-						<nuxt-link :to="{ name: 'weather' }">
-							{{ UserLocation.place_name }}
-						</nuxt-link>
-					</template>
-				</AppNavbar>
-			</header>
+		<AppNavbar>
+			<template #home>
+				Weather App
+			</template>
+			<template #location-settings>
+				<location-settings-dialog
+					:location="UserLocation"
+					:locations="getLocationHistory"
+					:use-search-a-p-i="getMapboxSearchResponse"
+				/>
+			</template>
+			<template #weather>
+				<nuxt-link :to="{ name: 'weather' }">
+					{{ UserLocation.place_name }}
+				</nuxt-link>
+			</template>
+		</AppNavbar>
+	</header>
 </template>
 
 <script lang="ts" setup>
